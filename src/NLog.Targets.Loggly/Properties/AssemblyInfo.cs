7180d7f3f691e74773f2dead24bfc22aafd94bc9
neutmute <neutmute@neutmute.nm>
2014-10-22 22:40:41 +1100

    nuspec version

--- src/NLog.Targets.Loggly/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.Loggly/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NLog.Targets.Loggly")]
 [assembly: AssemblyProduct("NLog.Targets.Loggly")]
 [assembly: AssemblyCopyright("Copyright © Joe Fitzgerald 2012")]
-[assembly: AssemblyDescription("A custom target for NLog allowing you to send log messages to loggly.com using the loggly-csharp library")]
+[assembly: AssemblyDescription("A custom target for NLog that sends log messages to loggly.com using the loggly-csharp library")]
 [assembly: AssemblyVersion("4.5.0.0")]
-[assembly: AssemblyInformationalVersion("4.5.0-alpha")]
+[assembly: AssemblyInformationalVersion("4.5.0-alpha-v2")]
 
 #if DEBUG
     [assembly: AssemblyConfiguration("Debug")]
