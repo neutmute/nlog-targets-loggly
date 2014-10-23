7ca3726d4e27baacf85c15cbc65efa6b408356d4
neutmute <neutmute@neutmute.nm>
2014-10-23 22:52:39 +1100

    better demo

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -8,27 +8,60 @@ using NLog;
 
 namespace Demo
 {
+    public class Vehicle
+    {
+        public string Name { get; set; }
+        public int Wheels { get; set; }
+
+        public int EngineSize { get; set; }
+    }
+
+    public class Truck : Vehicle
+    {
+        public Truck()
+        {
+            Wheels = 10;
+            EngineSize = 12;
+            Name = "Mack Truck";
+        }
+    }
+
     class Program
     {
         private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        
         static void Main(string[] args)
         {
-            // Normal logging
-            Log.Info("Demo starting up using loggly transport={0}", LogglyConfig.Instance.Transport);
+            StandardLogging();
+            StandardLogException();
 
-            // Adding custom parameter
+            LogWithObjectMetadata();
+            LogExceptionWithObjectMetadata();
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+        
+        private static void StandardLogging()
+        {
+            Log.Info("NLog.Targarts.Loggly demo starting up using loggly transport={0}", LogglyConfig.Instance.Transport);
+        }
+        private static void StandardLogException()
+        {
+            Log.ErrorException("Did someone unplug the cable?", new Exception("Cable unplugged"));
+        }
+        private static void LogWithObjectMetadata()
+        {
             var eventInfo = new LogEventInfo(LogLevel.Info, "MyLogName", "event info test message");
-            eventInfo.Properties.Add("CustomParameter", 123456);
+            eventInfo.Properties.Add("VehicleParameter", new Truck());
             Log.Log(eventInfo);
-
-            // Logging an exception
+        }
+        private static void LogExceptionWithObjectMetadata()
+        {
             var eventInfo2 = new LogEventInfo(LogLevel.Error, "MyLogName", "something broke");
             eventInfo2.Exception = new Exception("This is a really bad exception");
             eventInfo2.Properties.Add("CustomParameter", 987654);
             Log.Log(eventInfo2);
-
-            Console.WriteLine("Press any key...");
-            Console.ReadKey();
         }
     }
 }
