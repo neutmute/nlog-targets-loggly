6c524d8fad536ef22c2ae85d3101627099f8d4fd
neutmute <neutmute.email>
2014-10-24 20:31:16 +1100

    reference latest packages

--- src/NLog.Targets.Loggly/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.Loggly/Properties/AssemblyInfo.cs
@@ -7,10 +7,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Joe Fitzgerald 2012")]
 [assembly: AssemblyDescription("A custom target for NLog that sends log messages to loggly.com using the loggly-csharp library")]
 [assembly: AssemblyVersion("4.5.0.0")]
-[assembly: AssemblyInformationalVersion("4.5.0-alpha-v4")]
 
 #if DEBUG
     [assembly: AssemblyConfiguration("Debug")]
+    [assembly: AssemblyInformationalVersion("4.5.1-alpha-v4")]
 #else
     [assembly: AssemblyConfiguration("Release")]
+    [assembly: AssemblyInformationalVersion("4.5.0")]
 #endif
