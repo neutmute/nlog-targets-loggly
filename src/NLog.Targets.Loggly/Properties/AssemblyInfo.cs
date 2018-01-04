60e13ca22c034d7e62eacf8e16f924fcb979c87e
Rolf Kristensen <sweaty1@hotmail.com>
2018-01-04 19:48:16 +0100

    Upgrade to VS2017 and NetStandard2.0

--- src/NLog.Targets.Loggly/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.Loggly/Properties/AssemblyInfo.cs
@@ -2,15 +2,4 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NLog.Targets.Loggly")]
-[assembly: AssemblyProduct("NLog.Targets.Loggly")]
-[assembly: AssemblyDescription("A custom target for NLog that sends log messages to loggly.com using the loggly-csharp library")]
-[assembly: AssemblyVersion("4.5.2.0")]
-
-#if DEBUG
-    [assembly: AssemblyConfiguration("Debug")]
-    [assembly: AssemblyInformationalVersion("4.5.2-alpha-v4")]
-#else
-    [assembly: AssemblyConfiguration("Release")]
-    //[assembly: AssemblyInformationalVersion("4.5.2")]
-#endif
+[assembly: ComVisible(false)]
